f7b6fc7897009ac5261586063f7d6b157c020829
hyeonw8 <whgusrud223@gmail.com>
2024-05-23 12:10:33 +0900

    ex1 과제 제출

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,14 +1,14 @@
 import { Link, useParams } from 'react-router-dom';
 
 const Detail = () => {
-  const { id } = useParams();
+  const params = useParams();
 
-  console.log(id);
+  console.log(params.id);
 
   return (
     <>
       <div>Detail Page입니다.</div>
-      <div>path: /detail/{id}</div>
+      <div>path: /detail/{params.id}</div>
       <Link to={`/`}>Home Page로 이동하기</Link>
     </>
   );
